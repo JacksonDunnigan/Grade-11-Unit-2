abd1516159d6f37c76e106cd0380c405fed1ca76
s241722 <s241722@shha216m28.ocsb.local>
2018-09-18 09:52:05 -0400

    it had an error earlier

--- ICS3UC_Unit2/src/activity3/ThreeDigit.java
+++ ICS3UC_Unit2/src/activity3/ThreeDigit.java
@@ -10,14 +10,8 @@ public class ThreeDigit {
 		
 		int num1 = ((input%1000)-(input%100))/100;
 		int num2 = ((input%100)-(input%10))/10;
-		int num3 = (input%10);//-input;
-		//double num1=input%10;
-		//double num2=((input%100)%10);//-num1;
-		//double num3=((input%1000)%100);
-		
-		int sum=num1+num2+num3;//+num2;//+num3;
-		
-		
+		int num3 = (input%10);
+		int sum=num1+num2+num3;
 		Console.print("The sum of your numbers is: "+sum);
 	}
 }
