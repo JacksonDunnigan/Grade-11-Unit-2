a1fd6e21e6bcd0f8c0f95745f94573f4135057cd
s241722 <s241722@shha216m28.ocsb.local>
2018-09-20 10:08:06 -0400

    i fixed some issues

--- ICS3UC_Unit2/src/activity3/ThreeDigit.java
+++ ICS3UC_Unit2/src/activity3/ThreeDigit.java
@@ -8,7 +8,7 @@ public class ThreeDigit {
 		
 		int input = Console.readInt("Enter the a number:");
 		
-		int num1 = ((input%1000)-(input%100))/100;
+		int num1 = (input-(input%100))/100;
 		int num2 = ((input%100)-(input%10))/10;
 		int num3 = (input%10);
 		int sum=num1+num2+num3;
--- ICS3UC_Unit2/src/activity5/NumbersSum.java
+++ ICS3UC_Unit2/src/activity5/NumbersSum.java
@@ -3,14 +3,17 @@ package activity5;
 import simpleIO.Console;
 
 public class NumbersSum {
-	
+
 	public static void main(String[] args) {
-		//input
-		int input=Console.readInt("Enter a number:");
-		
-		//output
-		for (int i=0; i<input+1; i++) {
+		// input
+		int input = Console.readInt("Enter a number:");
+		int sum = 0;
+		// output
+		for (int i = 0; i < input + 1; i++) {
+			sum += i;
 			Console.print(i);
 		}
+		// outputs the sum
+		Console.print(sum);
 	}
 }
